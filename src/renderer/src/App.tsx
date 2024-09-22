dad6a3ad362d4e5d999727651586861423d4f9ee
Saurabh Mehta <saurabhm@melio.tech>
2024-09-23 00:22:03 +0530

    refactor: remove unused asset files

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,5 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
 
 function App(): JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
-
   return (
     <div>
       <h1 className='my-8 font-bold text-4xl text-blue-500 text-center '>Hello from Electron</h1>
