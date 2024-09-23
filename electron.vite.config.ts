50d8e4a690c9d68abdd4ae019595d82c2956a214
Saurabh Mehta <saurabhm@melio.tech>
2024-09-23 12:08:43 +0530

    feat: add path alias with typescript

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
+    resolve: {
+      alias: {
+        '@/lib': resolve('src/main/lib'),
+        '@shared': resolve('src/shared')
+      }
+    }
   },
   preload: {
     plugins: [externalizeDepsPlugin()]
@@ -12,7 +18,10 @@ export default defineConfig({
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'),
+        '@shared': resolve('src/shared'),
+        '@/components': resolve('src/rendered/src/components'),
+        '@/assets': resolve('src/rendered/src/assets'),
       }
     },
     plugins: [react()]
