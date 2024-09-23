e50a5d080393df39d5a98450629aac8ce8e186df
Saurabh Mehta <saurabhm@melio.tech>
2024-09-23 11:22:18 +0530

    feat: configure main script

--- src/main/index.ts
+++ src/main/index.ts
@@ -9,11 +9,13 @@ function createWindow(): void {
     width: 900,
     height: 670,
     show: false,
+    transparent: true,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
-      sandbox: false
+      sandbox: true,
+      contextIsolation: true
     }
   })
 
