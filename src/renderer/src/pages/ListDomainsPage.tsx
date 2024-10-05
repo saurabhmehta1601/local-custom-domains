d7de4f132257fae99561d64f577da5af9a4f89c7
Mayank mehta <114612710+mayankmehta19032003@users.noreply.github.com>
2024-10-05 18:08:31 +0530

    Display message when no custom local domain is created. (#8)
    * update form label color to white
    
    * display message if there is no domain created
    
    * Revert "display message if there is no domain created"
    
    This reverts commit 983e545fd1f8a4d478b34b759ab57e0ea848367f.
    
    * display banner message when no domain availabe
    
    * fix last commit display banner message

--- src/renderer/src/pages/ListDomainsPage.tsx
+++ src/renderer/src/pages/ListDomainsPage.tsx
@@ -1,6 +1,14 @@
 import { PageLayout, Table } from "@/components"
 import { useEffect, useState } from "react"
 
+
+const styles = {
+    noDomainsBannerContainer: "w-96 p-16 border-2 border-white rounded-lg mx-auto mt-32",
+    noDomainHeadingText: "text-center",
+    domainDataTable: "mt-8 w-fit mx-auto"
+
+}
+
 const ListDomainsPage = () => {
     const [allDomains, setAllDomains] = useState<{ name: string, port: string }[]>([])
     const [error, setError] = useState("")
@@ -29,11 +37,21 @@ const ListDomainsPage = () => {
     }, [])
     return (
         <PageLayout>
-            <Table
-                className="mt-8 w-fit mx-auto"
-                headers={["DOMAIN", "PORT"]}
-                rows={allDomains.map((domain) => ([domain.name, domain.port]))}
-            />
+            {
+                allDomains.length > 0 ? 
+                <Table
+                    className={styles.domainDataTable}
+                    headers={["DOMAIN", "PORT"]}
+                    rows={allDomains.map((domain) => ([domain.name, domain.port]))} 
+                />
+                :
+                <div className={styles.noDomainsBannerContainer}>
+                    <h5 className={styles.noDomainHeadingText}>
+                        No local custom domain created yet...
+                    </h5>
+                </div>
+            }
+
         </PageLayout>
     )
 }
