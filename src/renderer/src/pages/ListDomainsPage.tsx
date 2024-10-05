5700bb4ae775cea3581943d2e70eb145ffa179aa
Mayank Mehta <mayankmehta880@gmail.com>
2024-10-05 17:55:10 +0530

    display banner message when no domain availabe

--- src/renderer/src/pages/ListDomainsPage.tsx
+++ src/renderer/src/pages/ListDomainsPage.tsx
@@ -1,6 +1,14 @@
 import { PageLayout, Table } from "@/components"
 import { useEffect, useState } from "react"
 
+
+const styles = {
+    noDomainsBannerContainer: "w-96 p-16 border-2 border-white rounded-lg mx-auto mt-32",
+    noDomainHeadingText: "text-center",
+    domainDataTable: "mt-8 w-fit mx-auto"
+
+}
+
 const ListDomainsPage = () => {
     const [allDomains, setAllDomains] = useState<{ name: string, port: string }[]>([])
     const [error, setError] = useState("")
@@ -29,11 +37,16 @@ const ListDomainsPage = () => {
     }, [])
     return (
         <PageLayout>
-            <Table
-                className="mt-8 w-fit mx-auto"
-                headers={["DOMAIN", "PORT"]}
-                rows={allDomains.map((domain) => ([domain.name, domain.port]))}
-            />
+            {
+                allDomains.length > 0 ? 
+                <Table
+                    className={styles.domainDataTable}
+                    headers={["DOMAIN", "PORT"]}
+                    rows={allDomains.map((domain) => ([domain.name, domain.port]))} 
+                />
+                
+            }
+
         </PageLayout>
     )
 }
