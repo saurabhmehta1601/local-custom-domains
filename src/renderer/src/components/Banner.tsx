07b4134637a5b22cbdc5d93e3ca4efd1df1b546a
Mayank Mehta <mayankmehta880@gmail.com>
2024-10-19 22:38:50 +0530

    add toast notification on get started also

--- src/renderer/src/components/Banner.tsx
+++ src/renderer/src/components/Banner.tsx
@@ -1,30 +1,40 @@
-import { GetStartedButton, ReadDocsButton } from '@/components'
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { GetStartedButton, ReadDocsButton } from '@/components';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const styles = {
   banner: 'justify-center items-center',
   btnGroup: 'flex gap-x-8 justify-center items-center',
   bannerText:
     'w-fit mx-auto mt-48 mb-12 text-center bg-clip-text text-transparent bg-gradient-to-r from-slate-300 to-slate-500'
-}
+};
 
 const Banner = () => {
+  const showToast = () => {
+    toast.info('This feature is under development.');
+  };
+
   return (
     <div className={styles.banner}>
       <div>
         <h3 className={styles.bannerText}>Local Custom Domains</h3>
       </div>
       <div className={styles.btnGroup}>
-        <GetStartedButton />
 
-        <div onClick={() => toast.info('This feature is under development.')}>
+        <button onClick={showToast}>
+          <GetStartedButton />
+        </button>
+
+        <button onClick={showToast}>
           <ReadDocsButton />
-        </div>
+        </button>
+        
         <ToastContainer position="bottom-right" />
+
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
+
